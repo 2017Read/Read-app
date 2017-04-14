276f27d60e1322799c5273232d14c21b1f0f2882
651619709@qq.com <651619709@qq.com>
2017-04-14 14:22:35 +0800

    V2.1

---
+++ js/H-main-1.js
@@ -0,0 +1,22 @@
+//$(document).ready(function(){
+//	$('.H-banner-box')[0].addEventListener("touchmove",function(e){
+//		e.preventDefault();
+//		let x=e.changedTouches[0].pageX;
+//		let n=e.changedTouches[0].clientX;
+//		let c=0;
+//		function move(){
+//			if(n>$('.H-banner-active-2').width()){
+//				c=n-$('.H-banner-active-2').width();
+//			}else{
+//				c=$('.H-banner-active-2').width()-n;
+//				$('.H-banner-active-2').css({
+//					left:-c+30
+//				})
+//			}
+//			console.log(n)
+//
+//		}
+//		move()
+//	})
+//})
+//
