93ef60c0b3f3a4c5bd013740db11517c82c9ce13
651619709@qq.com <651619709@qq.com>
2017-04-14 14:46:59 +0800

    V2.3“

--- js/H-main-1.js
+++ js/H-main-1.js
@@ -1,22 +1,27 @@
-//$(document).ready(function(){
-//	$('.H-banner-box')[0].addEventListener("touchmove",function(e){
-//		e.preventDefault();
-//		let x=e.changedTouches[0].pageX;
-//		let n=e.changedTouches[0].clientX;
-//		let c=0;
-//		function move(){
-//			if(n>$('.H-banner-active-2').width()){
-//				c=n-$('.H-banner-active-2').width();
-//			}else{
-//				c=$('.H-banner-active-2').width()-n;
-//				$('.H-banner-active-2').css({
-//					left:-c+30
-//				})
-//			}
-//			console.log(n)
-//
-//		}
-//		move()
-//	})
-//})
-//
+	<script type="text/javascript">
+	function isPassive() {
+    var supportsPassiveOption = false;
+    try {
+        addEventListener("test", null, Object.defineProperty({}, 'passive', {
+            get: function () {
+                supportsPassiveOption = true;
+            }
+        }));
+    } catch(e) {}
+    return supportsPassiveOption;
+}
+	function loaded () {
+	myScroll = new IScroll('#wrapper', {
+		
+		mouseWheel: true,
+		interactiveScrollbars: true,
+		shrinkScrollbars: 'clip',
+		fadeScrollbars: true,
+		resizeScrollbars:true,
+	});
+}
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
+	capture: false,
+	passive: false,
+} : false);
+</script>
