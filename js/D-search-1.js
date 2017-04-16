f9e98196307e8286b27f124207b9c1752fd81d26
dfflove <1148894034@qq.com>
2017-04-16 17:57:30 +0800

    dff

--- js/D-search-1.js
+++ js/D-search-1.js
@@ -8,4 +8,38 @@ window.onload=function(){
 		fadeScrollbars: true,
 		resizeScrollbars:true,
 	});
+	
+	$('.D-title > div:nth-child(4)').click(function(){
+		$(".D-qingkong").remove();
+	})
+	
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+	
+//	搜索重绘
+	$("#H-search").focus(function(){
+		$(".D-focus").remove();
+//		$('#H-search').keyup(function(){
+//			let value=$(this).val();
+//			let arr=[];
+//			$(".H-renqi-titleBox .title-1").each(function(){	
+//				let name=$(this).attr("value");
+//				arr.push(name);				
+//			})
+//			if(arr.indexOf(value)!=-1){
+//				$(this).parent().parent().show();
+//			}else{
+//				$(this).parent().parent().hide();
+//			}
+//			
+//		})
+	})
+	
+	
+	
+	
+	
+	
 }
